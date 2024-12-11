edc7d277153bdb463dfde92a5350a895d70d5e0b
Ameliaf0 <amfi5890@colorado.edu>
2024-12-10 23:40:23 -0700

    formatting

---
+++ ica/final/js/main.js
@@ -0,0 +1,24 @@
+fetch('projects.json')
+    .then(response => response.json())
+    .then(data => {
+        displayProjects(data.projects);
+        console.log("working")
+    })
+    .catch(error => console.error('Error fetching the JSON:', error));
+
+    function displayProjects(projects) {
+        const projectsContainer = document.getElementById('projects');
+        projects.forEach(project => {
+            const projectElement = document.createElement('div');
+            projectElement.className = 'project';
+    
+            projectElement.innerHTML = `
+                <h2>${project.title}</h2>
+                <p>${project.description}</p>
+                <img src="${project.image}" alt="${project.title}">
+                <a href="${project.link}" target="_blank">View Project</a>
+            `;
+    
+            projectsContainer.appendChild(projectElement);
+        });
+    }    
